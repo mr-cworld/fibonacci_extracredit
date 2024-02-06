dcdfaa1fc6cc733b3d1857a913ff7fd0e5b1ab29
Michael <mcirino@luc.edu>
2024-02-06 10:29:46 -0600

    had some github command issues, curious to see how this push looks on webite also updated the code so that the loop works, its ugly, but works

--- fibonacci.java
+++ fibonacci.java
@@ -1,7 +1,6 @@
 // Initial Code from the Online GDB link // 
 
 import java.util.Scanner;
-
 public class fibonacci
 {
 	public static void main(String[] args) {
@@ -10,15 +9,34 @@ public class fibonacci
 	    number of sequences
 	    
 	    */
+        //Variables
+        String userinput = "";
+
+        // create Scanner
+        Scanner scnr = new Scanner(System.in);
+        
+        
+
+
 		 int n1=0,n2=1,n3,i,count=10;    
          System.out.print(n1+" "+n2);//printing 0 and 1    
-            
+       
+    while (!userinput.equals("N")) {
          for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
          {    
               n3=n1+n2;    
               System.out.print(" "+n3);    
               n1=n2;    
               n2=n3;    
-         }   
+         }  
+         
+         System.out.print("Would you like to select a new number? (N to end):");
+         userinput = scnr.nextLine();
+    }
+
+
+
+       scnr.close();  
 	}
+  
 }

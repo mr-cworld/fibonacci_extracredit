7a5b9e27be9cd8e618ca442cac03d7f18a96fd66
Michael <mcirino@luc.edu>
2024-02-06 11:10:09 -0600

    major push, finalized project. > Made the fibonacci print into a function for fun > needed help to check if the N was lowercase it would still work > had a major bug with the scnr eating a new line charector making it go infinite! > fun project

--- fibonacci.java
+++ fibonacci.java
@@ -11,32 +11,65 @@ public class fibonacci
 	    */
         //Variables
         String userinput = "";
+        int sequenceCount = 1; //starting variable for how many times to run the sequence
 
         // create Scanner
         Scanner scnr = new Scanner(System.in);
-        
-        
 
 
-		 int n1=0,n2=1,n3,i,count=10;    
-         System.out.print(n1+" "+n2);//printing 0 and 1    
-       
-    while (!userinput.equals("N")) {
-         for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
+        //PreLoop - giving better intro 
+        System.out.println("Welcome to the Fibonacci Simulator, take a look at it for the first 10 numbers!");
+		int n1=0,n2=1,n3,i,count=10;    
+        System.out.print(n1+" "+n2);//printing 0 and 1 
+        
+        for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
          {    
               n3=n1+n2;    
               System.out.print(" "+n3);    
               n1=n2;    
               n2=n3;    
-         }  
+         }     
+       
+
+    while (!userinput.equals("N")) {
+        
+        System.out.println();
+        System.out.print("Would you like to select a new number? (N to end): ");
+        userinput = scnr.nextLine().toUpperCase(); //googled the toUpperCase as well instead of making more if/then statements
+
+       
+        if (userinput.equals("N")) {
+           System.out.println("Thank you for running the simulation!");
+            break;
+        }
+        else {
+           System.out.print("Great! How many numbers of the sequence do you want to see? ");
+           sequenceCount = scnr.nextInt();
+           scnr.nextLine(); // to consume the newline charector?? googled this
+        }
+
+        System.out.print("0 1 "); // start sequence manually
+
+        generateFibonacci(sequenceCount);
          
-         System.out.print("Would you like to select a new number? (N to end):");
-         userinput = scnr.nextLine();
+        
     }
 
 
 
        scnr.close();  
 	}
-  
+
+    public static void generateFibonacci(int sequenceCount) {
+	int n1=0,n2=1,n3,i;  
+    
+    for(i=2;i<sequenceCount;++i)
+    {    
+         n3=n1+n2;    
+         System.out.print(" "+n3);    
+         n1=n2;    
+         n2=n3;    
+    }  
+}
+
 }
